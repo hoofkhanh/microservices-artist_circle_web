e75f6d8edbbe9ca357d6d70e8b0078ad7ed8c3d6
Ho Tuan Khanh <hotuankhanh20112016@gmail.com>
2025-07-17 18:09:09 +0700

    test cicd

--- build-all-docker-files.ps1
+++ build-all-docker-files.ps1
@@ -1,9 +1,6 @@
-docker build -f services/microservices-artist_circle_web-artist/Dockerfile -t hoofkhanh/artist-service:1.0.0 .
-docker build -f services/microservices-artist_circle_web-config_server/Dockerfile -t hoofkhanh/config_server-service:1.0.0 .
-docker build -f services/microservices-artist_circle_web-discovery/Dockerfile -t hoofkhanh/discovery-service:1.0.0 .
-docker build -f services/microservices-artist_circle_web-gateway/Dockerfile -t hoofkhanh/gateway-service:1.0.0 .
-docker build -f services/microservices-artist_circle_web-search/Dockerfile -t hoofkhanh/search-service:1.0.0 .
-docker build -f services/microservices-artist_circle_web-user/Dockerfile -t hoofkhanh/user-service:1.0.0 .
-
-
-
+docker build -f services/microservices-artist_circle_web-artist/Dockerfile -t hoofkhanh/microservices-artist_circle_web-artist:1.0.0 .
+docker build -f services/microservices-artist_circle_web-config_server/Dockerfile -t hoofkhanh/microservices-artist_circle_web-config_server:1.0.0 .
+docker build -f services/microservices-artist_circle_web-discovery/Dockerfile -t hoofkhanh/microservices-artist_circle_web-discovery:1.0.0 .
+docker build -f services/microservices-artist_circle_web-gateway/Dockerfile -t hoofkhanh/microservices-artist_circle_web-gateway:1.0.0 .
+docker build -f services/microservices-artist_circle_web-search/Dockerfile -t hoofkhanh/microservices-artist_circle_web-search:1.0.0 .
+docker build -f services/microservices-artist_circle_web-user/Dockerfile -t hoofkhanh/microservices-artist_circle_web-user:1.0.0 .
